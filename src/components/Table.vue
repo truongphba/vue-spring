dda49ae33acbc2b7a33bb274fef1b5ada60f41e5
truongphba <truongph.ba@gmail.com>
2021-06-01 18:22:07 +0700

    truongph

---
+++ src/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+    <b-pagination
+        align="right"
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+    ></b-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  // props: {
+  //   items: {
+  //     type: [],
+  //     default: []
+  //   }
+  // },
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' }
+      ],
+      perPage: 3,
+      currentPage: 2
+    }
+  },
+  computed: {
+    rows() {
+      return this.items.length
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Student.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>List Student</h2>
+    <Table></Table>
+  </div>
+</template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  name: "Student",
+  components: {
+    Table
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
